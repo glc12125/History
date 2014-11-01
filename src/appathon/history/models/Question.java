20f60d6bd4b5e032f08f47d006b921c6d9a181a6
yimai <yimai@eduroam-244-060.eng.cam.ac.uk>
2014-11-01 16:18:11 +0000

    revised Question

--- src/appathon/history/models/Question.java
+++ src/appathon/history/models/Question.java
@@ -3,41 +3,20 @@ import java.util.ArrayList;
 
 public class Question {
 	
+	public String kind = "ChooseOption";  // ChooseCountry, ChooseCity, ChooseOption
 	public String question = null;
 	public String imgUrl = null;
 	public ArrayList<Answer> options = new ArrayList<Answer>();
-	public int answerKey = -1;
+	public String correctAnswer = null;
 	public Answer country = null;
 	
-	public String kind(){
-		if(this.options.isEmpty()){
-			return "ChooseCountry";
-		}
-		else{
-			if(this.options.get(0).isGeographic){
-				return "ChooseCity";
-			}
-			else{
-				return "ChooseOption";
-			}
-		}
-	}
-	
-	public boolean isAnswerCorrect(int answerKey){
-		return answerKey == this.answerKey;
-	}
-	
-	public boolean isAnswerCorrect(String country){
-		return this.country != null && country == this.country.answer;
-	}
-
 	public Question(String question, String imgUrl, ArrayList<Answer> options,
-			int answerKey) {
+			String correctAnswer) {
 		super();
 		this.question = question;
 		this.imgUrl = imgUrl;
 		this.options = options;
-		this.answerKey = answerKey;
+		this.correctAnswer = correctAnswer;
 	}
 	
 }
