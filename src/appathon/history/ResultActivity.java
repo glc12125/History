dc475f9882e0c627b3a477561e5db65e23395e54
103969491 <gch1221@gmail.com>
2014-11-01 17:38:31 +0000

    result acitivity test

--- src/appathon/history/ResultActivity.java
+++ src/appathon/history/ResultActivity.java
@@ -59,10 +59,16 @@ public class ResultActivity extends Activity
 		users.add(new User());
 		users.get(0).setAvatar(R.drawable.avatar_chao_gao);
 		users.get(0).setName("Chao Gao");
-		users.get(0).setAvatar(R.drawable.avatar_chao_gao);
+		users.get(0).setScore(53);
+		users.get(1).setAvatar(R.drawable.avatar_meng_zhang);
 		users.get(1).setName("Meng Zhang");
+		users.get(1).setScore(14);
+		users.get(2).setAvatar(R.drawable.avatar_liangchuan_gu);
 		users.get(2).setName("Liangchuan Gu");
+		users.get(2).setScore(22);
+		users.get(3).setAvatar(R.drawable.avatar_yimai_fang);
 		users.get(3).setName("Yimai Fang");
+		users.get(3).setScore(31);
 
 		return users;
 	}
