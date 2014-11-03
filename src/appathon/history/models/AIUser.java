24565a3523adeb9cd63f62cbc4c5309cab310ab7
mzhang001 <zhangmeng1010@gmail.com>
2014-11-03 19:30:58 +0000

    AIUser

---
+++ src/appathon/history/models/AIUser.java
@@ -0,0 +1,42 @@
+package appathon.history.models;
+
+import java.util.Random;
+
+import appathon.qa.Question;
+
+public class AIUser extends User {
+
+	private double correctPercentage; // To determine the probability AI can correctly answer one question, 0.0 - 1.0
+	private double second_mean; // Decide the average time one AI responses 
+	private double second_std; // Decide the variance of responding time
+	private double second_this_turn; // Seconds this AI responds question this turn
+	
+	public AIUser(String name, boolean isAI, int avatar, int small_avatar) {
+		super(name, isAI, avatar, small_avatar);
+	}
+	
+	public AIUser(String name, int avatar, int small_avatar, 
+			double correctPercentage, double second_mean, double second_std) {
+		super(name, true, avatar, small_avatar);
+		this.correctPercentage = correctPercentage;
+		this.second_mean = second_mean;
+		this.second_std = second_std;
+		this.second_this_turn = sampleSecond();
+	}
+
+	public double sampleSecond() {
+		Random r = new Random();
+		return Math.abs(r.nextGaussian() * second_mean + second_std);
+	}
+	
+	public double getSecondThisTurn() {
+		return second_this_turn;
+	}
+	
+	public String sendResult(Question question, String selectedAnswer)
+	// Make an answer to one question
+	{
+		return null;
+	}
+	
+}
