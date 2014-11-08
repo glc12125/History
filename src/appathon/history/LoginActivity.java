99f1f24da912d2b53bbe93e7a6dcf97cde5ccce7
103969491 <gch1221@gmail.com>
2014-11-08 13:09:01 +0000

    ~

--- src/appathon/history/LoginActivity.java
+++ src/appathon/history/LoginActivity.java
@@ -2,7 +2,10 @@ package appathon.history;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -11,16 +14,24 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.facebook.FacebookException;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
 import com.facebook.widget.FacebookDialog;
+import com.facebook.widget.FriendPickerFragment;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.PickerFragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
@@ -83,6 +94,9 @@ public class LoginActivity extends FragmentActivity
 
 		// for facebook
 		showHashKey(this);
+
+		LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+		loginButton.setReadPermissions("user_friends");
 	}
 
 	public static void showHashKey(Context context)
@@ -110,7 +124,84 @@ public class LoginActivity extends FragmentActivity
 	{
 		super.onActivityResult(requestCode, resultCode, data);
 		uiHelper.onActivityResult(requestCode, resultCode, data, dialogCallback);
+		pickFriends();
+	}
+
+	private void pickFriends()
+	{
+		final FriendPickerFragment fragment = new FriendPickerFragment();
+
+		setFriendPickerListeners(fragment);
+
+		showPickerFragment(fragment);
+	}
+
+	private void showPickerFragment(FriendPickerFragment fragment)
+	{
+		fragment.setOnErrorListener(new PickerFragment.OnErrorListener()
+		{
+			@Override
+			public void onError(PickerFragment<?> pickerFragment,
+					FacebookException error)
+			{
+				String text = "exception" + error.getMessage();
+				Toast toast = Toast.makeText(LoginActivity.this, text,
+						Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		});
+
+		FragmentManager fm = getSupportFragmentManager();
+		fm.beginTransaction().replace(R.id.fragment_container, fragment)
+				.addToBackStack(null).commit();
+		fm.executePendingTransactions();
+
+		fragment.loadData(true);
+	}
+
+	private void setFriendPickerListeners(final FriendPickerFragment fragment)
+	{
+		fragment.setOnDoneButtonClickedListener(new FriendPickerFragment.OnDoneButtonClickedListener()
+		{
+			@Override
+			public void onDoneButtonClicked(PickerFragment<?> pickerFragment)
+			{
+				onFriendPickerDone(fragment);
+			}
+		});
+	}
+
+	private void onFriendPickerDone(FriendPickerFragment fragment)
+	{
+		FragmentManager fm = getSupportFragmentManager();
+		fm.popBackStack();
+
+		String results = "";
+
+		List<GraphUser> selection = fragment.getSelection();
+
+		if (selection != null && selection.size() > 0)
+		{
+			ArrayList<String> names = new ArrayList<String>();
+			for (GraphUser user : selection)
+			{
+				names.add(user.getName());
+			}
+			results = TextUtils.join(", ", names);
+		} else
+		{
+			results = "no_friends_selected";
+		}
+
+		showAlert("you_picked", results);
+
 		startActivity(new Intent(this, MainActivity.class));
+	}
+
+	private void showAlert(String title, String message)
+	{
+		new AlertDialog.Builder(this).setTitle(title).setMessage(message)
+				.setPositiveButton("ok", null).show();
 	}
 
 	private void onSessionStateChange(Session session, SessionState state,
